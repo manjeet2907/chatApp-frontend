72445b72e5c640a4835db0b4e3e254c9ad28888f
Manjeet Saini <manjeet29saini@gmail.com>
2022-12-05 18:16:02 +0530

    updating cors

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://chatapp-backend07.herokuapp.com/",
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://chatapp-api-vhsl.onrender.com/",
+  // baseURL: "http://127.0.0.1:5000",
 });
 
 export default instance;
